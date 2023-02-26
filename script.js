71f651cad4acacca1ec95693ad79665b2b0d09c6
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-25 18:54:12 -0700

    Added a function to API call weather data

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ var humidElm = document.getElementById('humiditylevel');
 var windElm = document.getElementById('windspeed');
 
 
+
+//calls weather data to main card on click event
+searchBtn.addEventListener('click', function(event) {
+    
+
+    var city = searchTextInput.value;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // Extract the data from the API response
+            var city = data.name;
+            var temperature = data.main.temp;
+            var humidity = data.main.humidity;
+            var windSpeed = data.wind.speed;
+
+            // Update the HTML elements with the data
+            cityName.innerHTML = city;
+            tempElm.innerHTML = `Temperature: ${temperature}°C`;
+            humidElm.innerHTML = `Humidity: ${humidity}%`;
+            windElm.innerHTML = `Wind Speed: ${windSpeed}m/s`;
+        })
+       
+});
+
