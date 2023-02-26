32f66bf6d63f7a7bb8e73f5f16f5e00b650acfad
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-25 19:07:21 -0700

    reworked the function for error message appearing on html

--- script.js
+++ script.js
@@ -12,32 +12,29 @@ var windElm = document.getElementById('windspeed');
 
 //calls weather data to main card on click
 searchBtn.addEventListener('click', function(event) {
-    
     var city = searchTextInput.value;
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
         .then(function(response) {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
             return response.json();
         })
         .then(function(data) {
-            // Extract the data from the API response
             var city = data.name;
             var temperature = data.main.temp;
             var humidity = data.main.humidity;
             var windSpeed = data.wind.speed;
 
-            if (!city || !temperature || !humidity || !windSpeed) {
-                console.error("Error: Could not retrieve weather data.");
-                return;
-            }
-            
-            // Update the HTML elements with the data
             cityName.innerHTML = city;
             tempElm.innerHTML = `Temperature: ${temperature}°C`;
             humidElm.innerHTML = `Humidity: ${humidity}%`;
             windElm.innerHTML = `Wind Speed: ${windSpeed}m/s`;
         })
-        
+        .catch(function(error) {
+            cityName.innerHTML = "Error: Please Enter a Valid City Name ";
+        });
 });
 
