e1b6fd63facc1aaaf2b4adbe6ac5d350a8c33df4
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-25 18:33:08 -0700

    Prepped HTML structure for JS varaiables and created JS varaibles

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const API_KEY = "a54da92fedb6a7ab239ffa6b1965f74f";
+
+var searchBtn = document.getElementById('searchbtn')
+var searchTextInput = document.getElementById('searchbar')
+
+var cityName = document.getElementById('cityname');
+var tempElm = document.getElementById('templevel');
+var humidElm = document.getElementById('humiditylevel');
+var windElm = document.getElementById('windspeed');
+
+
